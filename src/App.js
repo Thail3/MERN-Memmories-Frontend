f380ae170f64b884295015563f4a04097b295107
Thail3 <tanai.saeng@gmail.com>
2022-06-17 22:04:06 +0700

    google login with redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Container } from "@material-ui/core";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { gapi } from "gapi-script";
 
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Auth from "./components/auth/Auth";
 
 function App() {
+  gapi.load("auth2", () => {
+    gapi.auth2.init({
+      client_id:
+        "316868431139-7qv71uqbhiemcjb7u110h2f4p790gd74.apps.googleusercontent.com",
+      scope: "https://www.googleapis.com/auth/userinfo.profile",
+    });
+  });
+
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -8,6 +8,10 @@ import {
   Container,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { GoogleLogin } from "react-google-login";
+import { useDispatch } from "react-redux";
+
+import Icon from "./icon";
 import useStyles from "./styles";
 
 import Input from "./Input";
@@ -15,6 +19,8 @@ import Input from "./Input";
 function Auth() {
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+
   const classes = useStyles();
 
   const handleSubmit = () => {};
@@ -30,15 +36,35 @@ function Auth() {
     setShowPassword(false);
   };
 
+  const googleSuccess = async (res) => {
+    console.log(res);
+
+    const result = res?.profileObj;
+    const token = res?.tokenId;
+
+    try {
+      dispatch({ type: "AUTH", data: { result, token } });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const googleFailure = (err) => {
+    console.log(err);
+    console.log("Google login failed");
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
+
         <Typography component="h1" variant="h5">
           {isSignup ? "Sign up" : "Sign in"}
         </Typography>
+
         <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             {isSignup && (
@@ -80,6 +106,7 @@ function Auth() {
               />
             )}
           </Grid>
+
           <Button
             type="submit"
             fullWidth
@@ -89,6 +116,27 @@ function Auth() {
           >
             {isSignup ? "Sign Up" : "Sign In"}
           </Button>
+
+          <GoogleLogin
+            clientId="316868431139-7qv71uqbhiemcjb7u110h2f4p790gd74.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<Icon />}
+                variant="contained"
+              >
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy="single_host_origin"
+          />
+
           <Grid container justify="flex-end">
             <Grid item>
               <Button onClick={switchMode}>
